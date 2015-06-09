d1c8820797df5ec4f70c42948e778946901a3186
ramusus <ramusus@gmail.com>
2015-06-09 18:17:12 +0300

    updated factory

--- vkontakte_users/factories.py
+++ vkontakte_users/factories.py
@@ -3,7 +3,9 @@ import factory
 import random
 
 class UserFactory(factory.DjangoModelFactory):
-    FACTORY_FOR = User
 
     remote_id = factory.Sequence(lambda n: n)
     sex = random.choice([1,2])
+
+    class Meta:
+        model = User
